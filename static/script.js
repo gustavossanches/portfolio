f7d312992fa531b1891b96a85331e60e9390ae50
gustavossanches <gustavo.sanches80@hotmail.com>
2025-01-14 12:20:42 -0300

    alteração no js

--- static/script.js
+++ static/script.js
@@ -1,3 +1,14 @@
+
+function rolarAteMinhaSecao(e) {
+  var botao = document.getElementById(e);
+  if (botao) {
+    botao.scrollIntoView({ behavior: 'smooth' });
+    botao.scrollTo()
+  }
+}
+
+
+
 // efeito de digitação do texto inicial
 const texto = 'Aqui você poderá saber um pouco mais sobre mim, o que estudo e como trabalho.'
 const tag = document.getElementById("description")
@@ -5,7 +16,7 @@ const tag = document.getElementById("description")
 const texto2 = 'Olá! Me chamo Gustavo e sou desenvolvedor.'
 const tag2 = document.getElementById("texto-principal")
 
-function efeitoTeclado(texto, tag, tempo, letraDelay, parar) {//adicionar mais um parametro e caso tenha ele, a bara do cursos some depois de um tempo para simular que foi para o outro paragrafo
+function efeitoTeclado(texto, tag, tempo, letraDelay, parar) {
   const arrayLetras = texto.split('')
 
   let cursor = document.createElement('span')
@@ -22,61 +33,44 @@ function efeitoTeclado(texto, tag, tempo, letraDelay, parar) {//adicionar mais u
 
   let textoAtual = ''
   let atrasoAcumulado = 0
-  let teste = 0
-
   arrayLetras.forEach((e, i) => {
-    // Se for a letra especificada, adiciona o atraso especial
     if (i === letraDelay) {
-      atrasoAcumulado += 3000 - tempo  // Adiciona apenas a diferença do atraso
+      atrasoAcumulado += 3000 - tempo  
     }
-    // Adiciona o atraso ao tempo do setTimeout
     setTimeout(() => {
       textoAtual += e
       tag.innerText = textoAtual
-      tag.appendChild(cursor)  // Reaplica o cursor, garantindo que ele permaneça visível
-      teste =  atrasoAcumulado + (tempo * i)
-      console.log('TESTE -> ', teste)
+      tag.appendChild(cursor)
     }, atrasoAcumulado + (tempo * i))
   })
 }
-  efeitoTeclado(texto, tag, 50, 7)
-  efeitoTeclado(texto2, tag2, 50, 4, 'teste')
-
-// setTimeout(() => {
-  //   efeitoTeclado(texto, tag, 50, 7)
-  //   efeitoTeclado(texto2, tag2, 50, 4, 'teste')
-  
-// }, 10000);
+  // efeitoTeclado(texto, tag, 50, 7)
+  efeitoTeclado(texto2, tag2, 50, 4)
 
 
 
 
-//HEADER
-//se estiver no topo ou se o mouse estiver em cima, fica colorido
-//se nao estiver no topo, fica opaco
 
-function rolarAteMinhaSecao(e) {
-  var botao = document.getElementById(e);
-  if (botao) {
-    botao.scrollIntoView({ behavior: 'smooth' });
-    botao.scrollTo()
-  }
-}
 
-const header = document.getElementsByTagName("header")[0];
-let mouseHeader = true
 
-header.addEventListener('mouseenter', () => {
-  mouseHeader = true
-  // console.log('entrou')
-  // console.log(mouseHeader)
 
-})
-header.addEventListener('mouseleave', () => {
-  mouseHeader = false
-  // console.log(mouseHeader)
-  // console.log('saiu')
-})
+
+//OPACIDADE DO HEADER CONFORME MOUSE E SCROLLY
+
+// const header = document.getElementsByTagName("header")[0];
+// let mouseHeader = true
+
+// header.addEventListener('mouseenter', () => {
+//   mouseHeader = true
+//   // console.log('entrou')
+//   // console.log(mouseHeader)
+
+// })
+// header.addEventListener('mouseleave', () => {
+//   mouseHeader = false
+//   // console.log(mouseHeader)
+//   // console.log('saiu')
+// })
 
 
 // window.onscroll = function () {
